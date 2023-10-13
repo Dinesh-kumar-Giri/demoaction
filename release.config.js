a2ff82e87179565372496d7aa49495a4b0bb50b2
dineshgiri <dineshgiri1542@gmail.com>
2023-10-13 23:51:23 +0530

    feat: changes he giri BREAKING CHANGE: hello giri

---
+++ release.config.js
@@ -0,0 +1,10 @@
+export const config={
+    "branches": ["main"],
+    "plugins": [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      "@semantic-release/github"
+    ]
+  }
+  
