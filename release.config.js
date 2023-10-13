277ecadc4f91f2571160a93494effc9601a03cc8
dineshgiri <dineshgiri1542@gmail.com>
2023-10-14 00:53:40 +0530

    feat: add video

--- release.config.js
+++ release.config.js
@@ -10,16 +10,38 @@
   
 
 
+// const config = {
+//     branches: ['main'],
+//     plugins: [
+//       '@semantic-release/commit-analyzer',
+//       '@semantic-release/release-notes-generator',
+//       '@semantic-release/npm',
+//       ['@semantic-release/git', {
+//         'assets': ['package.json'],
+//         'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+//       }],
+//       '@semantic-release/github'
+//     ]
+//   };
+
+
+
 const config = {
     branches: ['main'],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
       '@semantic-release/npm',
       ['@semantic-release/git', {
-        'assets': ['package.json'],
-        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }],
-      '@semantic-release/github'
+      ['@semantic-release/github', {
+        assets: '!dist/*' 
+      }]
     ]
   };
+  
+  module.exports = config;
+  
