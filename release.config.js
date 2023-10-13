4f080177aa3b4b9524a786473ac06ef8d69db354
dineshgiri <dineshgiri1542@gmail.com>
2023-10-14 00:57:24 +0530

    feat:update logic

--- release.config.js
+++ release.config.js
@@ -7,8 +7,6 @@
 //       "@semantic-release/github"
 //     ]
 //   }
-  
-
 
 // const config = {
 //     branches: ['main'],
@@ -24,24 +22,21 @@
 //     ]
 //   };
 
-
-
 const config = {
-    branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/npm',
-      ['@semantic-release/git', {
-        assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }],
-      ['@semantic-release/github', {
-        assets: '!dist/*' 
-      }]
-    ]
-  };
-  
-  module.exports = config;
-  
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
+
+module.exports = config;
