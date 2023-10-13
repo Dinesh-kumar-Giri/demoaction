d9c39c5ffb833a97c63a4a11b07db6a884d3c2e1
dineshgiri <dineshgiri1542@gmail.com>
2023-10-14 00:47:16 +0530

    feat: Changes welcome message
    BREAKING
    CHNAGE:
    MESSAGE changed
    from "Hello" to welcome

--- release.config.js
+++ release.config.js
@@ -1,10 +1,25 @@
-export const config={
-    "branches": ["main"],
-    "plugins": [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/github"
-    ]
-  }
+// export const config={
+//     "branches": ["main"],
+//     "plugins": [
+//       "@semantic-release/commit-analyzer",
+//       "@semantic-release/release-notes-generator",
+//       "@semantic-release/changelog",
+//       "@semantic-release/github"
+//     ]
+//   }
   
+
+
+const config = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/npm',
+      ['@semantic-release/git', {
+        'assets': ['package.json'],
+        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }],
+      '@semantic-release/github'
+    ]
+  };
